fb1717543b4a4bad72b52e50e9b715a12cc560bc
David Daraselia <d.daraselia@gmail.com>
2025-09-10 22:02:37 +0400

    cl

--- core/client.ts
+++ core/client.ts
@@ -1,4 +1,4 @@
-import { Message } from "./messages";
+import { Message } from "./types";
 
 export type SubscribeCallback = (message: Message) => void;
 export type Subscription = { unsubscribe: () => void };
