572f356626c19fe93541649be6039d7bfbba4661
David Daraselia <d.daraselia@gmail.com>
2025-09-27 22:35:53 +0400

    use-melony-message

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./melony-provider";
 export * from "./use-melony-messages";
+export * from "./use-melony-message";
 export * from "./use-melony-part";
 export * from "./use-melony-send";
 export * from "./use-melony-status";
